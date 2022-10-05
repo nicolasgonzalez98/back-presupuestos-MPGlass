80a15b22ceebcf6570c14b2c82b0f5f5a27899fb
unknown <nicolasgonzalez470@gmail.com>
2022-10-05 01:51:49 -0300

    client model

---
+++ src/models/Client.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('client', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        surname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dni: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING(100)
+        },
+        phone_number: {
+            type: DataTypes.STRING
+        }
+    })
+}
--- src/models/Users.js
+++ src/models/Users.js
@@ -4,9 +4,17 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('users', {
-    name: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 };
