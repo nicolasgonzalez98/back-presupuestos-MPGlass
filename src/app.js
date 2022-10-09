1397e751a5bff035b3f88b5bec03e1aec4a3a3c0
unknown <nicolasgonzalez470@gmail.com>
2022-10-09 03:14:38 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
+  console.log('hola')
   res.status(status).send(message);
 });
 
